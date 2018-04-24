28beee7632ac58d9f70981bfe50071d432e9a7f1
Josip Dorvak <jdorvak@gmail.com>
2018-04-24 07:38:12 -0400

    Added tempmute command

--- index.js
+++ index.js
@@ -41,57 +41,5 @@ bot.on("message", async message => {
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
 
     if(commandfile) commandfile.run(bot, message, args);
-
-    /* if(cmd === `${prefix}kick`){
-        let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!kUser) return message.channel.send("Couldn't find user.");
-        let reason = args.join(" ").slice(22);
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nice Try!");
-        if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked");
-
-        let kickEmbed = new Discord.RichEmbed()
-        .setDescription("~Kick~")
-        .setColor("#e56b00")
-        .addField("Kicked User", `${kUser} with ID: ${kUser.id}`)
-        .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-        .addField("Time", message.createdAt)
-        .addField("Reason", reason);
-
-        let kickchannel = message.guild.channels.find(`name`, "incidents");
-        if(!kickchannel) return message.channel.send("Cant find correct channel.");
-
-        message.guild.member(kUser).kick(reason).catch(e => {
-            return message.channel.send(`Could not kick user due to error: ${e.message}.`);
-        });
-        kickchannel.send(kickEmbed);
-
-        return;
-    }
-
-    if(cmd === `${prefix}serverinfo`){
-        let sicon = message.guild.iconURL;
-        let serverembed = new Discord.RichEmbed()
-        .setDescription("Server Information")
-        .setColor("#15f153")
-        .setThumbnail(sicon)
-        .addField("Server Name", message.guild.name)
-        .addField("Created On", message.guild.createdAt)
-        .addField("You Joined", message.member.joinedAt)
-        .addField("Total Members", message.guild.memberCount);
-
-        return message.channel.send(serverembed);
-    }
-
-    if(cmd === `${prefix}botinfo`){
-        let bicon = bot.user.displayAvatarURL;
-        let botembed = new Discord.RichEmbed()
-        .setDescription("Bot Information")
-        .setColor("#15f153")
-        .setThumbnail(bicon)
-        .addField("Bot Name", bot.user.username)
-        .addField("Created On", bot.user.createdAt);
-
-        return message.channel.send(botembed);
-    } */
 });
 bot.login(tokenfile.token);
